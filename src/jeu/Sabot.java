31eef9eb6fd288388e4c44d920bab9826bdab467
Jorian Cantalejo <cnj3887a@ensfsi.onmicrosoft.com>
2024-02-14 17:02:12 +0100

    tp 1 suite

--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -1,9 +1,43 @@
 package jeu;
 
-public class Sabot {
+import java.util.ConcurrentModificationException;
 
-	public Sabot() {
-		// TODO Auto-generated constructor stub
+import cartes.Carte;
+
+public class Sabot{
+	private int nombreCartesMax;
+	Carte[] cartes;
+	private int nbCartes;
+	
+	public Sabot(int nombreCartesMax, int nbCartes) {
+		this.nombreCartesMax=nombreCartesMax;
+		Carte[] cartes= new Carte[nombreCartesMax];
+		this.nbCartes=nbCartes;
 	}
-
+	public boolean estVide() {
+		return (nbCartes==0);
+	}
+	private void ajouterCarte(Carte carte) {
+		try {
+			cartes[nbCartes]=carte;
+			nbCartes+=1;
+		}catch (NullPointerException e) {
+			System.out.println("dépassement de capacité.");
+		}
+	}
+	public void ajouterFamilleCarte(Carte[] tabcarte) {
+		try {
+			for (int i=0;i<tabcarte.length;i++) {
+				ajouterCarte(tabcarte[i]);
+			}
+		}catch (IllegalStateException e) {
+			
+		}catch (ConcurrentModificationException j) {
+			
+		}
+	}
+	public void remove() {
+		cartes[nbCartes]=null;
+		nbCartes-=1;
+	}
 }
