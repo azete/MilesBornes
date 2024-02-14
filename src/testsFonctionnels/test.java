bc36eddbe4c527e6d2f1c5007d1eada99cd0160a
Jorian Cantalejo <cnj3887a@ensfsi.onmicrosoft.com>
2024-02-14 17:45:05 +0100

    tp 1 suite

--- src/testsFonctionnels/test.java
+++ src/testsFonctionnels/test.java
@@ -1,9 +1,14 @@
 package testsFonctionnels;
 
+import cartes.Carte;
+import cartes.Attaque;
+import cartes.Probleme;
+import jeu.Sabot;
+import cartes.Bataille;
+import cartes.Borne;
 public class test {
-
-	public test() {
-		
+	public static void main(String[] args) {
+		Borne borne= new Borne(3,50);
+		Sabot sabot=new Sabot(150,0);
 	}
-
 }
